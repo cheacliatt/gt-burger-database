792065081865df3dea9c357e98d1387b05a1cdb3
CheaC <cheacliatt@gmail.com>
2020-07-13 16:53:45 -0400

    Seeds folder

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Normal Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Veggie Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Crazy Burger");

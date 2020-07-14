0ad19e8ffa79c006b8d2cda3041b235f39680ba4
CheaC <cheacliatt@gmail.com>
2020-07-13 22:33:15 -0400

    Added partial handlebars, add runs, not satisfied yet

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-    burger.all(function(data) {
+    burger.selectAll(function(data) {
       var hbsObject = {
         burgers: data
       };
@@ -13,7 +13,7 @@ router.get("/", function(req, res) {
   });
   
   router.post("/api/burgers", function(req, res) {
-    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
@@ -24,7 +24,7 @@ router.get("/", function(req, res) {
   
     console.log("condition", condition);
   
-    burger.update(
+    burger.updateOne(
       {
         devoured: req.body.devoured
       },
